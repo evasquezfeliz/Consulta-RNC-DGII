57954d0d797a98afe3ac1dd7c73fbde41b35fd80
evasquezfeliz <49447622+evasquezfeliz@users.noreply.github.com>
2023-07-19 20:10:50 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 
 namespace ApiDGII1
 {
@@ -9,23 +8,20 @@ namespace ApiDGII1
     {
         static void Main(string[] args)
         {
-            // Create a new HttpClient instance.
-            HttpClient client = new HttpClient();
-
-            // Set the Content-Type header to application/json.
-            client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+            // Create a new HttpRequest instance.
+            HttpRequest request = new HttpRequest();
 
             // Set the Request URL.
-            string requestUrl = "https://dgii.gob.do/ws/v2/rnc/consultar";
+            request.RequestUri = "https://dgii.gob.do/ws/v2/rnc/consultar";
 
-            // Create a new HttpRequestMessage instance.
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, requestUrl);
+            // Set the Content-Type header to application/json.
+            request.Headers.Add(HttpRequestHeader.ContentType, "application/json");
 
             // Set the RNC number in the request body.
             request.Content = new StringContent("RNC=123456789-0", Encoding.UTF8, "application/json");
 
             // Send the request and get the response.
-            HttpResponseMessage response = client.Send(request);
+            HttpResponseMessage response = WebRequest.Http.Send(request);
 
             // Check the response status code.
             if (response.StatusCode == HttpStatusCode.OK)
